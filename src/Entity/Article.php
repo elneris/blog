e1619001f9366c31cf871a4e797a49ce50b1cd1c
Elneris Dang <elneris.dang@gmail.com>
2019-06-02 00:04:07 +0200

    quete 18

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,9 +5,15 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ * @UniqueEntity(
+ *     fields={"title"},
+ *     message="ce titre existe déjà"
+ * )
  */
 class Article
 {
@@ -20,11 +26,19 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="/\b(digital)\b/i",
+     *     match=false,
+     *     message="en français, il faut dire numérique"
+     * )
      */
     private $content;
 
