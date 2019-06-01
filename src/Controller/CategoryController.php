05b971803566dcf21499238b9fd5e784c090df61
Elneris Dang <elneris.dang@gmail.com>
2019-06-01 15:24:10 +0200

    quete 17.2

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,6 +16,7 @@ class CategoryController extends AbstractController
     /**
      * @Route("Category", name="add_category")
      * @return Response A response instance
+     * @IsGranted("ROLE_ADMIN")
      */
     public function add(Request $request) : Response
     {
